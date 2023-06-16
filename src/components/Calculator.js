f55dd5cdcbf799397eae3ba50b08bedfb03822d7
porag-m06 <porag.m06@gmail.com>
2023-06-16 11:47:29 +0600

    testing button action

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,40 @@
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 export default function MyCalculator() {
+
+  const btnAction = (event)=>{
+    //calculate(event.target.textContent)
+    console.log(event,"\n\nThe button that is clicked is: ",event.target.textContent);
+  }
+
+
+
+
   return (
     <div className="calculator-component">
       <div className="calculator-sec">
         <input type="text" name="input" id="input" placeholder="0" />
         <div className="cal-btn">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="bk-org">÷</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="bk-org">x</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="bk-org">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="bk-org">+</button>
-          <button type="button" className="btn-0">0</button>
-          <button type="button">.</button>
-          <button type="button" className="bk-org">=</button>
+          <button onClick={btnAction} type="button">AC</button>
+          <button onClick={btnAction} type="button">+/-</button>
+          <button onClick={btnAction} type="button">%</button>
+          <button onClick={btnAction} type="button" className="bk-org">÷</button>
+          <button onClick={btnAction} type="button">7</button>
+          <button onClick={btnAction} type="button">8</button>
+          <button onClick={btnAction} type="button">9</button>
+          <button onClick={btnAction} type="button" className="bk-org">x</button>
+          <button onClick={btnAction} type="button">4</button>
+          <button onClick={btnAction} type="button">5</button>
+          <button onClick={btnAction} type="button">6</button>
+          <button onClick={btnAction} type="button" className="bk-org">-</button>
+          <button onClick={btnAction} type="button">1</button>
+          <button onClick={btnAction} type="button">2</button>
+          <button onClick={btnAction} type="button">3</button>
+          <button onClick={btnAction} type="button" className="bk-org">+</button>
+          <button onClick={btnAction} type="button" className="btn-0">0</button>
+          <button onClick={btnAction} type="button">.</button>
+          <button onClick={btnAction} type="button" className="bk-org">=</button>
         </div>
       </div>
     </div>
